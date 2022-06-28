adbe967eb12bb4485e29749caf17fe82ad67255d
Saul <saul_p4rdo@icloud.com>
2022-06-28 03:53:40 -0500

    Variables de entorno en next

--- pages/blog/[id].jsx
+++ pages/blog/[id].jsx
@@ -8,7 +8,8 @@ const EntradaBlog = ({ entrada }) => {
 };
 
 export async function getStaticPaths() {
-  const url = "http://localhost:1337/blogs";
+  const url = `${process.env.API_URL}/blogs`;
+
   const respuesta = await fetch(url);
   const entradas = await respuesta.json();
 
@@ -23,7 +24,7 @@ export async function getStaticPaths() {
   };
 }
 export async function getStaticProps({ params: { id } }) {
-  const url = `http://localhost:1337/blogs/${id}`;
+  const url = `${process.env.API_URL}/blogs/${id}`;
   console.log(url);
   const respuesta = await fetch(url);
   const entrada = await respuesta.json();
@@ -37,6 +38,7 @@ export async function getStaticProps({ params: { id } }) {
 }
 // export async function getServerSideProps({ query: { id } }) {
 //   const url = `http://localhost:1337/blogs/${id}`;
+//    const url = `${process.env.API_URL}/blogs/${id}`;
 //   console.log(url);
 //   const respuesta = await fetch(url);
 //   const entrada = await respuesta.json();
