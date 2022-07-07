99e2e7b0dbc5e47b2ea4ebee551f8d4c656a0a48
Saul <saul_p4rdo@icloud.com>
2022-07-07 03:55:53 -0500

    CSS al modelo de guitarra

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Footer from "./Footer";
 import Header from "./Header";
 
-const Layout = ({ children, pagina }) => {
+const Layout = ({ children, pagina, guitarra }) => {
   return (
     <div>
       <Head>
@@ -18,12 +18,16 @@ const Layout = ({ children, pagina }) => {
         />
       </Head>
 
-      <Header />
+      <Header guitarra={guitarra} />
       {children}
 
       <Footer />
     </div>
   );
+};
+
+Layout.defaultProps = {
+  guitarra: null,
 };
 
 export default Layout;
